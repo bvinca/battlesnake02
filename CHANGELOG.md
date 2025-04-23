5cc4cb7caeb8e457284766102d71e314820af1ee
albajgora <albajgora@york.citycollege.eu>
2025-04-24 00:49:56 +0200

    Feature: Add food targeting feature

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+
+## [Unreleased]
+
+### Added
+- Added a food targeting feature 
+
+### Changed
+- Refactored folder structure for clarity. 
+
+### Fixed
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import runServer from './server.js';
 import { checkSelfCollision } from './src/selfCollision.js';
 import { checkOtherSnakesCollision } from './src/otherSnakesCollision.js';
 import { checkWallCollision } from './src/wallCollision.js';
+import { findClosestFood, getDirectionToFood } from './src/foodTargeting.js';
 
 // info is called when you create your Battlesnake on play.battlesnake.com
 // and controls your Battlesnake's appearance
@@ -30,66 +31,80 @@ function end(gameState) {
 }
 
 // move is called on every turn and returns your next move
+
 function move(gameState) {
-  let isMoveSafe = {
-    up: true,
-    down: true,
-    left: true,
-    right: true
-  };
+    let isMoveSafe = {
+        up: true,
+        down: true,
+        left: true,
+        right: true
+    };
 
-  // Prevent moving backwards
-  const myHead = gameState.you.body[0];
-  const myNeck = gameState.you.body[1];
+    // Prevent moving backwards
+    const myHead = gameState.you.body[0];
+    const myNeck = gameState.you.body[1];
 
-  if (myNeck.x < myHead.x) {        // Neck is left of head, don't move left
-    isMoveSafe.left = false;
-  } else if (myNeck.x > myHead.x) { // Neck is right of head, don't move right
-    isMoveSafe.right = false;
-  } else if (myNeck.y < myHead.y) { // Neck is below head, don't move down
-    isMoveSafe.down = false;
-  } else if (myNeck.y > myHead.y) { // Neck is above head, don't move up
-    isMoveSafe.up = false;
-  }
+    if (myNeck.x < myHead.x) {
+        isMoveSafe.left = false;
+    } else if (myNeck.x > myHead.x) {
+        isMoveSafe.right = false;
+    } else if (myNeck.y < myHead.y) {
+        isMoveSafe.down = false;
+    } else if (myNeck.y > myHead.y) {
+        isMoveSafe.up = false;
+    }
 
-  // Check all collision types
-  isMoveSafe = checkWallCollision(
-    myHead,
-    gameState.board.width,
-    gameState.board.height,
-    isMoveSafe
-  );
+    // Check collisions
+    isMoveSafe = checkWallCollision(
+        myHead,
+        gameState.board.width,
+        gameState.board.height,
+        isMoveSafe
+    );
 
-  isMoveSafe = checkSelfCollision(
-    myHead,
-    gameState.you.body,
-    isMoveSafe
-  );
+    isMoveSafe = checkSelfCollision(
+        myHead,
+        gameState.you.body,
+        isMoveSafe
+    );
 
-  isMoveSafe = checkOtherSnakesCollision(
-    myHead,
-    gameState.board.snakes,
-    gameState.you.id,
-    isMoveSafe
-  );
+    isMoveSafe = checkOtherSnakesCollision(
+        myHead,
+        gameState.board.snakes,
+        gameState.you.id,
+        isMoveSafe
+    );
 
-  // Are there any safe moves left?
-  const safeMoves = Object.keys(isMoveSafe).filter(key => isMoveSafe[key]);
-  if (safeMoves.length === 0) {
-    console.log(`MOVE ${gameState.turn}: No safe moves detected! Moving down`);
-    return { move: "down" };
-  }
+    // Find safe moves
+    const safeMoves = Object.keys(isMoveSafe).filter(key => isMoveSafe[key]);
+    if (safeMoves.length === 0) {
+        console.log(`MOVE ${gameState.turn}: No safe moves detected! Moving down`);
+        return { move: "down" };
+    }
 
-  // Choose a random move from the safe moves
-  const nextMove = safeMoves[Math.floor(Math.random() * safeMoves.length)];
+    // Food targeting logic
+    const closestFood = findClosestFood(myHead, gameState.board.food);
+    if (closestFood) {
+        const foodDirections = getDirectionToFood(myHead, closestFood);
 
-  console.log(`MOVE ${gameState.turn}: ${nextMove}`);
-  return { move: nextMove };
+        // Prioritize directions that move toward food
+        for (const direction of foodDirections) {
+            if (safeMoves.includes(direction)) {
+                console.log(`MOVE ${gameState.turn}: Moving toward food - ${direction}`);
+                return { move: direction };
+            }
+        }
+    }
+
+    // If no food-directed moves are safe, choose randomly from remaining safe moves
+    const nextMove = safeMoves[Math.floor(Math.random() * safeMoves.length)];
+    console.log(`MOVE ${gameState.turn}: ${nextMove}`);
+    return { move: nextMove };
 }
 
 runServer({
-  info: info,
-  start: start,
-  move: move,
-  end: end
+    info: info,
+    start: start,
+    move: move,
+    end: end
 });
