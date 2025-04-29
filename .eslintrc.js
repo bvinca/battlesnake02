8c0d39981a91da6df262bcb8e44e9be76024257f
bvinca <bvinca@york.citycollege.eu>
2025-04-29 19:17:41 +0300

    add .gitignore file

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    extends: [
+      'eslint:recommended',
+      'plugin:sonarjs/recommended',
+      'plugin:unicorn/recommended',
+      'plugin:eslint-comments/recommended',
+      'prettier'
+    ],
+    plugins: [
+      'sonarjs',
+      'unicorn',
+      'eslint-comments'
+    ],
+    rules: {}
+  };
+  
